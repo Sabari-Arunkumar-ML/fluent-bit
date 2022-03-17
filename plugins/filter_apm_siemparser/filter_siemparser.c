544c63f6de7d738968332aaf25bc34a6cda9e8a6
Arun Kumar AR <Arun.KumarAR@Maplelabs.Com>
2022-03-17 15:12:43 +0530

    initilazed varable with null values during declaration for siem parser

--- plugins/filter_apm_siemparser/filter_siemparser.c
+++ plugins/filter_apm_siemparser/filter_siemparser.c
@@ -201,12 +201,13 @@ static int cb_modifier_filter(const void* data, size_t bytes,
         kv = obj->via.map.ptr;
         int i = 0;
         int EventID = 0;
-        char* Sid;
-        double  time;
-        char* OfficeLogInTime;
-        char* OfficeLogOutTime;
-        char* message;
-        char* SendingMessage;
+        char* Sid = NULL;
+        double  time = 0;
+        char* targetAccountDomainName = NULL;
+        char* accountName = NULL;
+        char* accountDomain = NULL;
+        char* targetAccountName = NULL;
+        char* SendingMessage = NULL;
         int newEntries = 0;
         bool eventToProcess = false;
         for (i = 0; i < map_num; i++)
@@ -216,90 +217,101 @@ static int cb_modifier_filter(const void* data, size_t bytes,
             if (old_record_key->type == MSGPACK_OBJECT_STR && !strncasecmp(old_record_key->via.str.ptr, "EventID", 7))
             {
                 if (old_record_value->via.i64 == 4624 || old_record_value->via.i64 == 4625) {
-                    flb_info("Login Event");
                     newEntries = 4;
                     msgpack_pack_map(&packer, map_num + newEntries);
-                    EventID = old_record_value->via.i64;          
+                    EventID = old_record_value->via.i64;
                     eventToProcess = true;
+                    flb_info("EventID value %d", EventID);
+
                 }
                 if (old_record_value->via.i64 == 1033 || old_record_value->via.i64 == 1034) {
                     newEntries = 3;
                     msgpack_pack_map(&packer, map_num + newEntries);
                     EventID = old_record_value->via.i64;
                     eventToProcess = true;
+                    flb_info("EventID value %d", EventID);
+
                 }
                 if (old_record_value->via.i64 == 4720) {
                     newEntries = 3;
                     msgpack_pack_map(&packer, map_num + newEntries);
                     EventID = old_record_value->via.i64;
                     eventToProcess = true;
+                    flb_info("EventID value %d", EventID);
                 }
                 if (old_record_value->via.i64 == 4726) {
                     newEntries = 2;
                     msgpack_pack_map(&packer, map_num + newEntries);
                     EventID = old_record_value->via.i64;
                     eventToProcess = true;
+                    flb_info("EventID value %d", EventID);
+
                 }
                 if (old_record_value->via.i64 == 4616) {
                     newEntries = 4;
                     msgpack_pack_map(&packer, map_num + newEntries);
                     EventID = old_record_value->via.i64;
                     eventToProcess = true;
+                    flb_info("EventID value %d", EventID);
+
                 }
                 if (old_record_value->via.i64 == 104) {
                     newEntries = 2;
                     msgpack_pack_map(&packer, map_num + newEntries);
                     EventID = old_record_value->via.i64;
                     eventToProcess = true;
+                    flb_info("EventID value %d", EventID);
+
                 }
 
             }
             if (old_record_key->type == MSGPACK_OBJECT_STR && !strncasecmp(old_record_key->via.str.ptr, "Sid", 3))
             {
                 Sid = flb_strndup(old_record_value->via.str.ptr, old_record_value->via.str.size);
-                //flb_info("sid %s", Sid);
+                flb_info("sid value %s", Sid);
             }
-            if (old_record_key->type == MSGPACK_OBJECT_STR && !strncmp(old_record_key->via.str.ptr, "time", 4))
+            if (old_record_key->type == MSGPACK_OBJECT_STR && !strncmp(old_record_key->via.str.ptr, "targetAccountDomainName", 23))
             {
-                time = old_record_value->via.f64;
-                //flb_info("time %lu", time);
+                targetAccountDomainName = flb_strndup(old_record_value->via.str.ptr, old_record_value->via.str.size);
+                flb_info("targetAccountDomainName %s", targetAccountDomainName);
             }
-            if (old_record_key->type == MSGPACK_OBJECT_STR && !strncasecmp(old_record_key->via.str.ptr, "OfficeLogInTime", 15)) {
-                
-                OfficeLogInTime = flb_strndup(old_record_value->via.str.ptr, old_record_value->via.str.size);
-                ///*flb_info*/("logintime %s", OfficeLogInTime);
+            if (old_record_key->type == MSGPACK_OBJECT_STR && !strncasecmp(old_record_key->via.str.ptr, "accountName", 11)) {
+                accountName = flb_strndup(old_record_value->via.str.ptr, old_record_value->via.str.size);
+                flb_info("accountName %s", accountName);
             }
-            if (old_record_key->type == MSGPACK_OBJECT_STR && !strncasecmp(old_record_key->via.str.ptr, "OfficeLogOutTime", 16))
+            if (old_record_key->type == MSGPACK_OBJECT_STR && !strncasecmp(old_record_key->via.str.ptr, "accountDomain", 13))
             {
-                
-                OfficeLogOutTime = flb_strndup(old_record_value->via.str.ptr, old_record_value->via.str.size);
-               // flb_info("logouttime %s", OfficeLogOutTime);
+                accountDomain = flb_strndup(old_record_value->via.str.ptr, old_record_value->via.str.size);
+                flb_info("accountDomain %s", accountDomain);
             }
-            if (old_record_key->type == MSGPACK_OBJECT_STR && !strncasecmp(old_record_key->via.str.ptr, "message", 7)) {
-                message = flb_strndup(old_record_value->via.str.ptr, old_record_value->via.str.size);
-                while (message[i] != '\0')
-                {
-                    if (message[i] == '\t' || message[i] == '\r' || message[i] == '\n')
-                    {
-                         message[i] = ' ';
-                    }
-                    i++;
-                }
+            if (old_record_key->type == MSGPACK_OBJECT_STR && !strncasecmp(old_record_key->via.str.ptr, "targetAccountName", 17)) {
+                targetAccountName = flb_strndup(old_record_value->via.str.ptr, old_record_value->via.str.size);
+                flb_info("targetAccountName %s", targetAccountName);
             }
         }
         for (i = 0; i < map_num; i++) {
             msgpack_pack_object(&packer, (kv + i)->key);
             msgpack_pack_object(&packer, (kv + i)->val);
         }
-        if (eventToProcess == false){
+
+        if (eventToProcess == false) {
             continue;
         }
-        flb_info("************************");
-        flb_info("%d^%s^%s^%s^%s^%f", EventID, message, Sid, OfficeLogOutTime, OfficeLogInTime, time);
-        flb_info("************************");
+        if (EventID == 4624 || EventID == 4625) {
+            flb_info("%d^%s^%s", EventID, accountName, accountDomain);
+            sprintf(SendingMessage, "%d^%s^%s", EventID, accountName, accountDomain);
+        }
+        if (EventID == 4720) {
+            flb_info("%d^%s^%s", EventID, targetAccountName, targetAccountDomainName);
+            sprintf(SendingMessage, "%d^%s^%s", EventID, targetAccountName, targetAccountDomainName);
+        }
+        if (EventID == 1033 || EventID == 1034) {
+            flb_info("%d^%s", EventID, Sid);
+            sprintf(SendingMessage, "%d^%s", EventID, Sid);
+        }
 
         //SendingMessage = "4624^An account was successfully logged on.\\r\\n\\r\\nSubject:\\r\\n\\tSecurity ID : \\t\\tS - 1 - 5 - 18\\r\\n\\tAccount Name : \\t\\tXOR - BAN - 274$\\r\\n\\tAccount Domain : \\t\\tWORKGROUP\\r\\n\\tLogon ID : \\t\\t0x3e7\\r\\n\\r\\nLogon Information : \\r\\n\\tLogon Type : \\t\\t2\\r\\n\\tRestricted Admin Mode : \\t - \\r\\n\\tVirtual Account : \\t\\t % %1843\\r\\n\\tElevated Token : \\t\\t % %1842\\r\\n\\r\\nImpersonation Level : \\t\\t % %1833\\r\\n\\r\\nNew Logon : \\r\\n\\tSecurity ID : \\t\\tS - 1 - 5 - 21 - 2547325851 - 3128153700 - 699163993 - 1003\\r\\n\\tAccount Name : \\t\\tsrivathsa.mugalodi\\r\\n\\tAccount Domain : \\t\\tXOR - BAN - 274\\r\\n\\tLogon ID : \\t\\t0x1062789\\r\\n\\tLinked Logon ID : \\t\\t0x10627b6\\r\\n\\tNetwork Account Name : \\t - \\r\\n\\tNetwork Account Domain : \\t - \\r\\n\\tLogon GUID : \\t\\t{ 00000000 - 0000 - 0000 - 0000 - 000000000000 }\\r\\n\\r\\nProcess Information : \\r\\n\\tProcess ID : \\t\\t0x868\\r\\n\\tProcess Name : \\t\\tC:\\Windows\\System32\\svchost.exe\\r\\n\\r\\nNetwork Information : \\r\\n\\tWorkstation Name : \\tXOR - BAN - 274\\r\\n\\tSource Network Address : \\t127.0.0.1\\r\\n\\tSource Port : \\t\\t0\\r\\n\\r\\nDetailed Authentication Information : \\r\\n\\tLogon Process : \\t\\tUser32 \\r\\n\\tAuthentication Package : \\tNegotiate\\r\\n\\tTransited Services : \\t - \\r\\n\\tPackage Name(NTLM only) :\\t - \\r\\n\\tKey Length : \\t\\t0\\r\\n\\r\\nThis event is generated when a logon session is created.It is generated on the computer that was accessed.\\r\\n\\r\\nThe subject fields indicate the account on the local system which requested the logon.This is most commonly a service such as the Server service, or a local process such as Winlogon.exe or Services.exe.\\r\\n\\r\\nThe logon type field indicates the kind of logon that occurred.The most common types are 2 (interactive) and 3 (network).\\r\\n\\r\\nThe New Logon fields indicate the account for whom the new logon was created, i.e.the account that was logged on.\\r\\n\\r\\nThe network fields indicate where a remote logon request originated.Workstation name is not always availableand may be left blank in some cases.\\r\\n\\r\\nThe impersonation level field indicates the extent to which a process in the logon session can impersonate.\\r\\n\\r\\nThe authentication information fields provide detailed information about this specific logon request.\\r\\n\\t - Logon GUID is a unique identifier that can be used to correlate this event with a KDC event.\\r\\n\\t - Transited services indicate which intermediate services have participated in this logon request.\\r\\n\\t - Package name indicates which sub - protocol was used among the NTLM protocols.\\r\\n\\t - Key length indicates the length of the generated session key.This will be 0 if no session key was requested.\\r\\n\\n^ S - 1 - 5 - 21 - 3144977761 - 145940102 - 240959454 - 1239 ^ 9:30 ^ 17 : 00 ^ 1646655800440";
-        sprintf(SendingMessage, "%d^%s^%s^%s^%s^%f", EventID, message, Sid, OfficeLogOutTime, OfficeLogInTime, time);
+        //sprintf(SendingMessage, "%d^%s^%s^%s^%s^%f", EventID, message, Sid, OfficeLogOutTime, OfficeLogInTime, time);
         char* endln = "\n";
         strncat(SendingMessage, endln, strlen(endln));
         flb_info("[%s] Sending siem message: %s", PLUGIN_NAME, SendingMessage);
